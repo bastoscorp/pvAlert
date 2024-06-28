8d0603238097a620e273f244eb790f7d971529bb
bastien <bstgarcia@gmail.com>
2024-06-28 12:33:02 +0200

    add logging info

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import logging
 
+from logging.handlers import TimedRotatingFileHandler
 from config.config import Config
 from business.rulesManager import RulesManager
 
 
-logging.basicConfig(level=logging.INFO)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(logging.Formatter('%(asctime)s -- %(levelname)s -- %(message)s'))
+
+file_handler = logging.FileHandler('logs/pvAlerts.log')
+file_handler.setFormatter(logging.Formatter('%(asctime)s -- %(levelname)s -- %(message)s'))
+file_handler.handlers = TimedRotatingFileHandler('logs/pvAlerts.log', when='D', interval=30, encoding="utf-8")
+
+logging.basicConfig(handlers=[console_handler,file_handler],level=logging.INFO)
+
+
+logging.info("Start pvAlert Checking ....")
+
 
 config = Config("config.ini")
-
-
 conso_manager = RulesManager(config)
 conso_manager.control_status()
 conso_manager.control_rules()
