61b019f0d6b7cd28d5da1e3e7d703a151aa70f03
bastien <bstgarcia@gmail.com>
2024-06-28 15:10:05 +0200

    fix bug

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ console_handler.setFormatter(logging.Formatter('%(asctime)s -- %(levelname)s -- 
 import os
 dirname = os.path.dirname(__file__)
 filename = os.path.join(dirname, 'logs/pvAlerts.log')
-print(filename)
+
 file_handler = logging.FileHandler(filename)
 file_handler.setFormatter(logging.Formatter('%(asctime)s -- %(levelname)s -- %(message)s'))
-file_handler.handlers = TimedRotatingFileHandler('logs/pvAlerts.log', when='D', interval=30, encoding="utf-8")
+file_handler.handlers = TimedRotatingFileHandler(filename, when='D', interval=30, encoding="utf-8")
 
 logging.basicConfig(handlers=[console_handler, file_handler], level=logging.INFO)
 
@@ -22,7 +22,7 @@ logging.basicConfig(handlers=[console_handler, file_handler], level=logging.INFO
 logging.info("Start pvAlert Checking ....")
 
 
-config = Config("config.ini")
-conso_manager = RulesManager(config)
-conso_manager.control_status()
-conso_manager.control_rules()
+#config = Config("config.ini")
+#conso_manager = RulesManager(config)
+#conso_manager.control_status()
+#conso_manager.control_rules()
