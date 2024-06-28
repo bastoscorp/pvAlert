d868c25e7aeaf2898bd716751916535e8808a253
bastien <bstgarcia@gmail.com>
2024-06-28 13:38:03 +0200

    add log folder

---
+++ pvAlerts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash +x
+
+script_dir=`dirname $0`
+
+echo $script_dir
+                                                                                       
+source $script_dir/bin/activate
+
+python3 $script_dir/main.py
+
+source deactivate
