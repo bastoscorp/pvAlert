21018919e9f2013dbc91a5b3ffd976b355fcc0a9
bastien <bstgarcia@gmail.com>
2024-07-10 11:30:42 +0200

    add logfile suffix

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ filename = os.path.join(dirname, 'logs/pvAlerts.log')
 file_handler = logging.FileHandler(filename)
 file_handler.setFormatter(formatter)
 file_handler.handlers = TimedRotatingFileHandler(filename, when='d', interval=1, encoding="utf-8")
+file_handler.suffix = "%Y-%m-%d.log"
 file_handler.setLevel(logging.INFO)
 file_handler.addFilter(logging.Filter())
 logging.root.addHandler(file_handler)
