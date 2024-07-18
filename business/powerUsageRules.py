61fbe21018dc6ea7d6e1e1e8aa826a00053d2ad9
bastoscorp <70770115+bastoscorp@users.noreply.github.com>
2024-07-18 13:59:00 +0200

    update power rule

--- business/powerUsageRules.py
+++ business/powerUsageRules.py
@@ -1,7 +1,7 @@
 class PowerUsageRules:
     rules = [{'rule_id': 1,
               'rule_name': "Pool Heating Threshold",
-              'rule_remaining_power_value': 1000,
+              'rule_remaining_power_value': 1100,
               'rule_priority': 1,
               'action_threshold_crossed': 'enable',
               'action_device_name': 'PAC piscine',
